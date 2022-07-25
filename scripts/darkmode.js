6190c20e128a9c37bf0a13ddaf6cb82ce3017b9b
Berkay <102474166+doandroidsdreamof@users.noreply.github.com>
2022-07-25 18:45:53 +0300

    Fixed Darkmode

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,4 +1,4 @@
-let tl = gsap.timeline({ defaults: { ease: "Power3.easeOut", duration: 1 } });
+let tl = gsap.timeline({defaults: { ease: "Power3.easeOut", duration: 1 },});
 
 const createdActive = activeTasks;
 const createdCompleted = completedTasks;
@@ -39,77 +39,21 @@ const darkThemeDarkGrayishBlue = "hsl(233, 14%, 35%)";
 const darkThemeVeryDarkGrayishBlue = "hsl(237, 14%, 26%)";
 
 const mobileGroup = document.querySelector('.function__group__mobile')
+const mobileGroupDesktop = document.querySelector('.function')
 const mobileGroupToggled = document.querySelector('.function__group')
 const monbileBreak = window.matchMedia("(min-width: 720px)")
 
 
-
-window.addEventListener('resize',responsiveImage)
-window.onload = () =>{
+window.onload =  () =>{
+  darkmodeButton.classList.remove("lightModeOpen");
+  darkmodeButton.classList.add("darkModeOpen");
   responsiveImage()
-}
-function responsiveImage(){
-  if( darkModeClass.classList.contains("darkModeOpen")){
-    monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlDarkDesktop})` : cover.style.backgroundImage = `url(${urlDarkMobile})`  
-  }
-  if( darkModeClass.classList.contains("lightModeOpen")){
-    monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlLightDesktop})` : cover.style.backgroundImage = `url(${urlLightMobile})`  
-  }
+
+  
 
 }
-
-darkmodeButton.addEventListener("click", (e) => {
-  const target = e.target;
-  if (target.className.match(".darkModeOpen")) {
-    darkmodeButton.classList.add("lightModeOpen");
-    darkmodeButton.classList.remove("darkModeOpen");
-    
-    let tl = gsap.timeline({
-      defaults: { ease: "Power3.easeOut", duration: 1 },
-    });
-    tl.to(".background", {
-      "clip-path": "circle(160% at 0% 0%)",
-      backgroundColor: lightThemeLightGrayishBlue,
-      opacity: 1,
-      duration: 1,
-    });
-    
-    ligthMode();
-  } else if (target.className.match(".lightModeOpen")) {
-    darkmodeButton.classList.remove("lightModeOpen");
-    darkmodeButton.classList.add("darkModeOpen");
-    let tl = gsap
-      .timeline({
-        defaults: { ease: "Power3.easeOut", duration: 1, stagger: 2 },
-      })
-      .to(".background", {
-        "clip-path": "circle(20% at 0% 120%)",
-        clipPath: "inset(150% 0% 240%)",
-        webkitClipPath: "inset(150% 0% 0%)",
-        backgroundColor: darkThemeVeryDarkBlue,
-        opacity: 1,
-        duration: 1,
-      })
-      .to(".background", {
-        "clip-path": "circle(160% at 100% 90%)",
-        clipPath: "inset(150% 0% 240%)",
-        webkitClipPath: "inset(150% 0% 0%)",
-        backgroundColor: darkThemeVeryDarkBlue,
-        opacity: 1,
-        duration: 1,
-      });
-    darkMode();
-  }
-});
 
 function ligthMode() {
-  mobileGroupToggled.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
-  mobileGroup.style.background = `${lightThemeVeryLightGray}`;
-  cover.style.backgroundImage = `url(${urlLightDesktop})`;
-  darkmodeButton.src = `${urlImageMoon}`;
-  inputTop.style.background = `${lightThemeVeryLightGray}`;
-  inputTop.style.color = "black";
-  monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlLightDesktop})` : cover.style.backgroundImage = `url(${urlLightMobile})`  
   for (let i = 0; i < wrapperItems.children.length; i++) {
     wrapperItems.children[i].style.background = `${lightThemeVeryLightGray}`;
   }
@@ -120,31 +64,84 @@ function ligthMode() {
     textCreated[i].style.color = `${lightThemeVeryDarkGrayishBlue}`;
     textCreated[i].style.borderColor = `${lightThemeVeryLightGrayishBlue}`;
   }
+  mobileGroupToggled.style.background = `${lightThemeVeryLightGray}`;
+  mobileGroup.style.background = `${lightThemeVeryLightGray}`;
+  cover.style.backgroundImage = `url(${urlLightDesktop})`;
+
+  inputTop.style.color = "black";
+
 }
 
 function darkMode() {
-  mobileGroupToggled.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
-  mobileGroup.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
-  darkmodeButton.src = `${urlImageSun}`;
-  inputTop.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
-  inputTop.style.color = `${darkThemeLightGrayishBlue}`;
-  monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlDarkDesktop})` : cover.style.backgroundImage = `url(${urlDarkMobile})`  
-
   for (let i = 0; i < wrapperItems.children.length; i++) {
-    wrapperItems.children[
-      i
-    ].style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
+    wrapperItems.children[i].style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
   }
   for (let i = 0; i < container.children.length; i++) {
-    container.children[
-      i
-    ].style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
+    container.children[i].style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
   }
   for (let i = 0; i < textCreated.length; i++) {
     textCreated[i].style.color = "white";
     textCreated[i].style.borderColor = `${darkThemeDarkGrayishBlue}`;
   }
+  mobileGroupToggled.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
+  mobileGroup.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
+  cover.style.backgroundImage = `url(${urlDarkDesktop})`;
+  inputTop.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
+  inputTop.style.color = `${darkThemeLightGrayishBlue}`;
+
 }
+
+
+darkmodeButton.addEventListener("click", (e) => {
+  const target = e.target;
+  if (darkModeClass.classList.contains("darkModeOpen")) {
+    darkmodeButton.classList.add("lightModeOpen");
+    darkmodeButton.classList.remove("darkModeOpen");
+    darkmodeButton.src = `${urlImageMoon}`;
+    mobileGroupDesktop.style.background = `${lightThemeVeryLightGray}`
+    inputTop.style.background = `${lightThemeVeryLightGray}`;
+    inputTop.style.color = "black";
+    let tl = gsap.timeline({defaults: { ease: "Power3.easeOut", duration: 1, stagger: .2 },});
+    tl.to(".background", {
+      "clip-path": "circle(160% at 0% 0%)",
+      backgroundColor: lightThemeLightGrayishBlue,
+      opacity: 1,
+      duration: 1,
+    });
+    ligthMode();
+    monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlLightDesktop})` : cover.style.backgroundImage = `url(${urlLightMobile})`  
+   
+  } else if (darkModeClass.classList.contains("lightModeOpen")) {
+    darkmodeButton.classList.add("darkModeOpen");
+    darkmodeButton.classList.remove("lightModeOpen");
+    darkmodeButton.src = `${urlImageSun}`;
+    mobileGroupDesktop.style.background = `${darkThemeVeryDarkDesaturatedBlue}`
+    inputTop.style.background = `${darkThemeVeryDarkDesaturatedBlue}`;
+    inputTop.style.color = "white";
+
+    let tl = gsap.timeline({defaults: { ease: "Power3.easeOut", duration: 1, stagger: .2 },})
+      tl.to(".background", {
+        "clip-path": "circle(20% at 0% 120%)",
+        clipPath: "inset(150% 0% 240%)",
+        webkitClipPath: "inset(150% 0% 0%)",
+        backgroundColor: darkThemeVeryDarkBlue,
+        opacity: 1,
+        duration: 1,
+      })
+      tl.to(".background", {
+        "clip-path": "circle(160% at 100% 90%)",
+        clipPath: "inset(150% 0% 240%)",
+        webkitClipPath: "inset(150% 0% 0%)",
+        backgroundColor: darkThemeVeryDarkBlue,
+        opacity: 1,
+        duration: 1,
+      });
+      darkMode();
+      monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlDarkDesktop})` : cover.style.backgroundImage = `url(${urlDarkMobile})`  
+ 
+  
+  }
+});
 
 inputTop.addEventListener("keypress", (e) => {
   if (e.key === "Enter" && darkModeClass.classList.contains("lightModeOpen")) {
@@ -156,3 +153,14 @@ inputTop.addEventListener("keypress", (e) => {
   }
 });
 
+window.addEventListener('resize',responsiveImage)
+
+function responsiveImage(){
+  if( darkModeClass.classList.contains("darkModeOpen")){
+    monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlDarkDesktop})` : cover.style.backgroundImage = `url(${urlDarkMobile})`  
+  }
+  if( darkModeClass.classList.contains("lightModeOpen")){
+    monbileBreak.matches ?  cover.style.backgroundImage = `url(${urlLightDesktop})` : cover.style.backgroundImage = `url(${urlLightMobile})`  
+  }
+
+}
